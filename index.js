f156f65e4435a5d3206a09e62325de7b6d6297ee
Tom Gousby <gousbyjr@gmail.com>
2019-06-17 10:29:10 -0400

    React boilerplate

---
+++ index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class ShowWorkouts extends Component {
+    constructor(props) {
+        super(props);
+        
+    }
+    
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default ShowWorkouts;
