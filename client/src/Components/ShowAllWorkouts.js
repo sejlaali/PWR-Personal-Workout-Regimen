774de5b27fc54e827d03ca9c4db73d418cabd38f
Sejla Ali <sejlaali@gmail.com>
2019-06-17 13:01:33 -0400

    updated models.js and seed.js

--- client/src/Components/ShowAllWorkouts.js
+++ client/src/Components/ShowAllWorkouts.js
@@ -1,6 +1,26 @@
 import React, {Component} from 'react'
+import axios from 'axios'
 
 class ShowAllWorkouts extends Component {
-render() {
+    constructor(props){
+        super(props)
+
+        this.state = {
+
+        }
+    }
+
+
+async componentDidMount() {
 
 }
+render() {
+return (
+    <div>
+I show all the workouts
+    </div>
+)
+}
+}
+
+export default ShowAllWorkouts
