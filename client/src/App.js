bcea80a85f46d7f470b2c20aded557f1d0afd394
Sejla Ali <sejlaali@gmail.com>
2019-06-17 11:39:29 -0400

    created boiler plate for react/client side app

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+class App extends Component {
+constructor(props) {
+  super(props)
+}
+  render() {
+    return (
+<div>PWR App
+
+
+<Router to="/" component="LandingPage"></Router>
+<Router to="/home" component="ShowAllWorkout"></Router>
+</div>
+    )
+  }
+}
+
+
+export default App;
