991d549ce2513603912370be013aa743b2281393
Sejla Ali <sejlaali@gmail.com>
2019-06-18 13:47:36 -0400

    updated react components

--- client/src/Components/OneCategory.js
+++ client/src/Components/OneCategory.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 import {Link, Redirect} from 'react-router-dom'
+import PersonalWorkoutRegimen from './'
 
 class OneCategory extends Component {
 constructor(props) {
