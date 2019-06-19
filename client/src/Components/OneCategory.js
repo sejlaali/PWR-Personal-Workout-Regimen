36044f4760ec4ee49bf1bb03804fc345b94bde35
Sejla Ali <sejlaali@gmail.com>
2019-06-19 09:18:27 -0400

    updated Personal workout regimen component

--- client/src/Components/OneCategory.js
+++ client/src/Components/OneCategory.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react'
-import axios from 'axios'
-import {Link, Redirect} from 'react-router-dom'
-import PersonalWorkoutRegimen from './PersonalWorkoutRegimen';
+import React, { Component } from "react";
+import axios from "axios";
+import { Link, Redirect } from "react-router-dom";
+import PersonalWorkoutRegimen from "./PersonalWorkoutRegimen";
 
 class OneCategory extends Component {
   constructor(props) {
@@ -11,6 +11,7 @@ class OneCategory extends Component {
       workoutsArray: []
     };
   }
+
   async componentDidMount() {
     const workouts = await axios.get(
       `http://localhost:3001/workouts/category/${this.props.match.params.id}`
@@ -20,44 +21,37 @@ class OneCategory extends Component {
       workoutsArray
     });
   }
-  // this.handleClick = this.handleClick.bind(this)
-}
-async componentDidMount() {
-  const workouts = await axios.get(`http://localhost:3001/workouts/category/${this.props.match.params.id}`)
-const workoutsArray = workouts.data.oneWorkout
-this.setState({
-  workoutsArray
-})
-}
 
-handleClick = async (id) => {
+  handleClick = async (id) => {
     await axios.put(`http://localhost:3001/workouts/regimen/${id}`, {
-     regimen: true
-    })   
-}
-// /category/:categoryId/workouts/:id
+      regimen: true
+    });
+  };
 
   render() {
-    console.log(this.state.workoutsArray);
     return (
       <div>
-        
-{this.state.workoutsArray.map(workout => 
-  <div>
-   <Link to={`/category/${workout.categoryId}/workout/${workout.id}/edit`}><h3>{workout.name}</h3></Link>
-   <p>Description: {workout.description}</p>
-   <p>Duration: {workout.duration}</p>
-   <p>Difficulty: {workout.difficulty}</p>
-   <button onClick={this.handleClick(workout.id)}> Add to PWR </button>
-  </div>
-)}
+        {this.state.workoutsArray.map(workout => (
+          <div>
+            <Link
+              to={`/category/${workout.categoryId}/workout/${workout.id}/edit`}
+            >
+              <h3>{workout.name}</h3>
+            </Link>
+            <p>Description: {workout.description}</p>
+            <p>Duration: {workout.duration}</p>
+            <p>Difficulty: {workout.difficulty}</p>
+            <button onClick={this.handleClick(workout.id)}> Add to PWR </button>
+          </div>
+        ))}
 
-<Link to={`/workouts/create/${this.props.match.params.id}`}>
+        <Link to={`/workouts/create/${this.props.match.params.id}`}>
           <button>Create a new workout</button>
         </Link>
         <PersonalWorkoutRegimen />
       </div>
     );
   }
+}
 
 export default OneCategory;
